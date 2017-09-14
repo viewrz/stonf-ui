a81c137f1cdbbf3870a78584bcae3280865490b0
guillaumervls <guillaumervls@users.noreply.github.com>
2017-09-14 17:00:33 +0200

    fix redirects

--- scripts/generate-redirects.js
+++ scripts/generate-redirects.js
@@ -2,9 +2,13 @@
 const { writeFileSync } = require('fs');
 const { resolve } = require('path');
 
+if (!process.env.API_ENDPOINT) throw new Error('API_ENDPOINT env var not set');
+
+process.env.API_ENDPOINT = process.env.API_ENDPOINT.replace(/\/$/, '');
+
 const redirects = `
-/*      /index.html                         200
 /api/*  ${process.env.API_ENDPOINT}/:splat  200
+/*      /index.html                         200
 `;
 
 writeFileSync(resolve(__dirname, '../build/_redirects'), redirects);
